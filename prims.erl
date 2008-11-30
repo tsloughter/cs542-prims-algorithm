2f84e1c9547677027753e6f1ce272b70c15701ff
Tristan Sloughter <tristan@ir.iit.edu>
2008-11-30 16:48:00 -0600

    added design_doc and commented prims

--- prims.erl
+++ prims.erl
@@ -1,9 +1,16 @@
 -module (prims).
 -export ([start/1]).
 
+%%% A vertex contains a vertex number, a list of adjacent vertices
+%%% its minimum distance, its parent's vertex number, in the tree, 
+%%% its minimum adjacent list of vertices and boolean for if it is 
+%%% in the queue.
 -record (vertex, {num, adj_list=[], min_distance=inf, parent=null, 
                   min_adj_list=[], is_in_Q=true}).
 
+%%% Takes the name of the graph file
+%%% Populates a list of vertices by reading in the file
+%%% Finds and prints the minimum spanning tree
 start (Filename) ->
     {ok, File} = file:open (Filename, read),
     VertexList = populate_vertex_list (File),
@@ -13,37 +20,55 @@ start (Filename) ->
     %io:format ("~w~n", [Tree]),
     file:close (File).   
 
-print_path ([]) ->
-    ok;
-print_path ([H |T]) when H#vertex.parent == null ->
-    Cost = lists:foldr(fun(X, Y) -> X#vertex.min_distance+Y end, 0, T),
+
+print_path (List) ->
+    Cost = lists:foldr(fun(X, Y) -> 
+                               if
+                                   X#vertex.parent == null ->
+                                       Y;
+                                   true ->                                       
+                                       X#vertex.min_distance+Y 
+                               end 
+                       end, 
+                       0, List),
     io:format ("Cost of tree: ~w~n", [Cost]),
     io:format ("Edges picked:~n"),
-    print_path (T);
-print_path ([H |T]) ->
+    print_edges (List).
+
+print_edges ([]) ->
+    ok;
+print_edges ([H |T]) when H#vertex.parent == null ->
+    print_edges (T);
+print_edges ([H |T]) ->
     io:format ("(~w, ~w) cost: ~w~n", [H#vertex.parent, H#vertex.num, 
                                        H#vertex.min_distance]),
-    print_path (T).
+    print_edges (T).
 
 populate_vertex_list (File) ->
+    io:get_line (File, ''), %% throw away first line
     populate_vertex_list (File, []).
 populate_vertex_list (File, VertexList) ->
     case io:get_line (File, '') of
         eof ->
             VertexList;
         "\n" ->
+            %%% if line is blank, skip it
             populate_vertex_list (File, VertexList);
         Line ->
+            %%% parse out the vertex numbers and weight of the edges
             [{V1, _}, {V2, _}, {Weight, _}] = lists:map (fun (X) -> 
                                                                  string:to_integer(X) 
                                                          end, 
                                                          string:tokens (Line, " ")),
+            %%% add vertices and edges to the list of vertices
             NewVertexList = update_vertex_list (V1, V2, Weight, VertexList),
             NewNewVertexList = update_vertex_list (V2, V1, Weight, NewVertexList),
             %io:format ("~w ~w ~w~n", [V1, V2, Weight]),
             populate_vertex_list (File, NewNewVertexList)
     end.
 
+%%% Add vertex V1 to the list of vertices if it doesn't already exist
+%%% Also add the edge to that vertices list of adjacent nodes
 update_vertex_list (V1, V2, Weight, VertexList) ->
     update_vertex_list (V1, V2, Weight, VertexList, []).  
 
@@ -57,22 +82,30 @@ update_vertex_list (V1, V2, Weight,
 update_vertex_list (V1, V2, Weight, [H | T], NewVertexList) ->
     update_vertex_list (V1, V2, Weight, T, [H | NewVertexList]).
       
+%%% The main function for building the min spanning tree
+%%% Takes a queue of vector numbers and the list of vector records
+%%% 
 find_min_tree ([], VertexList) -> 
     VertexList;                           
 find_min_tree ([VertexNum | T], VertexList) ->    
+    %%% get vertex record and set that it is no longer in the queue
     {NewVertex, NewVertexList} = update_vertex(VertexNum, VertexList, false),
     case NewVertex#vertex.parent of
         null ->
+            %%% no parent? don't do anything
             NewNewVertexList1 = NewVertexList,
             nothing;
         ParentNum ->
+            %%% the vertex has a parent, update the min adj list for the vertex and the parent
             NewNewVertexList = update_min_adj_list (ParentNum, NewVertexList, VertexNum),
             NewNewVertexList1 = update_min_adj_list (VertexNum, NewNewVertexList, ParentNum)
     end,
+    %%% 
     {NewNewNewVertexList, NewMinQueue} = update_adjacent (NewVertex, NewVertex#vertex.adj_list, 
                                                        NewNewVertexList1, T),
     find_min_tree (NewMinQueue, NewNewNewVertexList).
 
+%%% Returns a vertex from the vertex list based on its vertex number
 get_vertex (VertexNum, []) ->
     #vertex {num=VertexNum};
 get_vertex (VertexNum, [#vertex{num=VertexNum} = V | _T]) ->
@@ -80,6 +113,7 @@ get_vertex (VertexNum, [#vertex{num=VertexNum} = V | _T]) ->
 get_vertex (VertexNum, [_H|T]) ->
     get_vertex (VertexNum, T).
 
+%%% Returns a vertex and sets that it is not in the queue anymore
 update_vertex (Vertex, VertexList, Value) ->
     update_vertex (Vertex, VertexList, Value, []).
 
@@ -92,24 +126,27 @@ update_vertex (Vertex, [#vertex{num=Vertex} = V | T],
 update_vertex (Vertex, [H|T], Value, NewVertexList) ->
     update_vertex (Vertex, T, Value, [H | NewVertexList]).
 
+%%% Recursives over the adjacent vertices of the given vertex
+%%% If the vertex is in the queue (has not been visited) and the 
+%%% weight of the edge to it is less than the current min distance 
+%%% set the parent of the adjacent vertex to the vertex passed in and 
+%%% the min distance of the adajacent vertex to the weight. Finally 
+%%% update the queue with the modified vertices and recurse.
 update_adjacent (_, [], VertexList, MinQueue) ->
     {VertexList, MinQueue};
 update_adjacent (Vertex, [{AdjVertexNum, Weight} | T], VertexList, MinQueue) ->
     AdjVertex = get_vertex (AdjVertexNum, VertexList),
     if
         AdjVertex#vertex.is_in_Q, Weight < AdjVertex#vertex.min_distance -> 
-            % set parent of adjacent to latest_addition
-            % set min_distance of adjacent to weight-function
-            % (latest_addition, adjacent)
-            % update adjacent in Q, order by min_distance
             {NewAdjVertex, NewVertexList} = set_parent_and_min_dist (AdjVertex#vertex.num, VertexList,
                                                      Vertex#vertex.num),             
-            NewMinQueue = update_queue (NewAdjVertex#vertex.num, NewAdjVertex#vertex.min_distance, lists:delete(NewAdjVertex#vertex.num, MinQueue)),
+            NewMinQueue = update_queue (NewAdjVertex#vertex.num, NewAdjVertex#vertex.min_distance, lists:delete(NewAdjVertex#vertex.num, MinQueue), NewVertexList),
             update_adjacent (Vertex, T, NewVertexList, NewMinQueue);
         true ->
             update_adjacent (Vertex, T, VertexList, MinQueue)
     end.
 
+%%% returns the weight of an edge
 get_edge_weight (Vertex, AdjVertexNum) ->
     case lists:keysearch (AdjVertexNum, 1, Vertex#vertex.adj_list) of
         {value, {_, Weight}} ->
@@ -118,6 +155,8 @@ get_edge_weight (Vertex, AdjVertexNum) ->
             0
     end.
 
+%%% Sets the parent of the adjacent vertex to the vertex passed in and 
+%%% the min distance of the adajacent vertex to the weight.
 set_parent_and_min_dist (AdjVertexNum, VertexList, VertexNum) ->
     set_parent_and_min_dist (AdjVertexNum, VertexList, VertexNum, []).    
 set_parent_and_min_dist (AdjVertexNum, [#vertex{num=AdjVertexNum} = V | T], 
@@ -128,19 +167,26 @@ set_parent_and_min_dist (AdjVertexNum, [#vertex{num=AdjVertexNum} = V | T],
 set_parent_and_min_dist (AdjVertexNum, [H|T], VertexNum, NewVertexList) ->    
     set_parent_and_min_dist (AdjVertexNum, T, VertexNum, [H | NewVertexList]).
 
-update_queue (VertexNum, MinDist, MinQueue) ->
-    update_queue (VertexNum, MinDist, MinQueue, []).
+%%% Updates the queue by inserting the vertexnum to its correct spot
+update_queue (VertexNum, MinDist, MinQueue, VertexList) ->
+    update_queue (VertexNum, MinDist, MinQueue, [], VertexList).
 
-update_queue (VertexNum, _, [], NewMinQueue) ->
+update_queue (VertexNum, _, [], NewMinQueue, _VertexList) ->
     NewMinQueue ++ [VertexNum];              
-update_queue (VertexNum, MinDist, [H|T], NewMinQueue) ->
+update_queue (VertexNum, MinDist, [H|T], NewMinQueue, VertexList) ->
+    Vertex = get_vertex (H, VertexList),
+    Weight = Vertex#vertex.min_distance,
     if
-        H == inf ; MinDist < H ->
+        Weight == inf ; MinDist < Weight ->
+            %%% if H is infinite or its distance is ver the min distance
+            %%% insert the vertex there in the queue
             NewMinQueue ++ [VertexNum] ++ [H|T];              
         true ->
-            update_queue (VertexNum, MinDist, T, NewMinQueue++[H])
+            update_queue (VertexNum, MinDist, T, NewMinQueue++[H], VertexList)
     end.
 
+%%% Adds the parent to the vertexes minimum adjacent list and 
+%%% returns the new list of vertices
 update_min_adj_list (ParentNum, VertexList, VertexNum) ->
     update_min_adj_list (ParentNum, VertexList, VertexNum, []).
 
